bb393149a127feb9c00b3f205240db1ba3f5cac0
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-10-17 15:54:23 +0200

    updated again

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -20,7 +20,8 @@ item_2 = result_2['name']
 # [item for item in list]
 # arg = argv[1]
 
-# names = [item['title'] for item in result if item['completed'] == True and item['userId'] == int(argv[1])]
+# names = [item['title'] for item in result if item['completed'] == True
+# and item['userId'] == int(argv[1])]
 # print(names)
 
 # for employee in result:
@@ -31,13 +32,14 @@ count = 0
 count_2 = 0
 
 for item in result:
-    if (item['completed'] == True or item['completed'] == False) and item['userId'] == int(argv[1]):
+    if (item['completed'] is True or item['completed'] is False) \
+            and item['userId'] == int(argv[1]):
         count_2 += 1
-    if item['completed'] == True and item['userId'] == int(argv[1]):
+    if item['completed'] is True and item['userId'] == int(argv[1]):
         count += 1
 print('Employee {} is done with tasks({}/{}):'.format(item_2, count, count_2))
 
 for item in result:
-    if item['completed'] == True and item['userId'] == int(argv[1]):
+    if item['completed'] is True and item['userId'] is int(argv[1]):
         print('\t', ' ', end='')
         print(item['title'])
