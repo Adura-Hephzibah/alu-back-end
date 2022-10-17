49bb3a8f363662773b25b8217271d5729e74e9cd
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-10-17 15:56:16 +0200

    chnages

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -17,17 +17,6 @@ result_2 = response_2.json()
 
 item_2 = result_2['name']
 
-# [item for item in list]
-# arg = argv[1]
-
-# names = [item['title'] for item in result if item['completed'] == True
-# and item['userId'] == int(argv[1])]
-# print(names)
-
-# for employee in result:
-#     print(employee)
-# print(result_2['name'])
-
 count = 0
 count_2 = 0
 
